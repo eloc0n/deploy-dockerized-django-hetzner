498251aef6ee0fb9b5f2f8ffb09c81c2dbdca307
eloc0n <johnisb.21@gmail.com>
2023-07-23 19:07:47 +0300

    Change debug mode configuration with env sercret

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ if ENV_PATH.exists():
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")) == "1"
 
 ALLOWED_HOSTS = []
 
