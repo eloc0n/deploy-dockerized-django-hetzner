d2fc606a0ada82641c8082abfff9bcd95faaeac9
eloc0n <johnisb.21@gmail.com>
2023-07-24 20:25:25 +0300

    Fix entrypoint gunicorn path

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 APP_PORT=${PORT:-8000}
 cd /app/
-/opt/venv/bin/python manage.py migrate
-/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm cfeblog.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+/opt/virtualenv/bin/python manage.py migrate
+/opt/virtualenv/bin/gunicorn --worker-tmp-dir /dev/shm cfeblog.wsgi:application --bind "0.0.0.0:${APP_PORT}"
