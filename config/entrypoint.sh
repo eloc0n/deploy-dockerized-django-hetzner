a229344c8361ae723cfbcff908d9e18e6be9b075
Giannis Bratias <giannis.bratias@grapevine.gr>
2023-07-26 21:46:04 +0300

    Add devops folder and move all configurations inside for django/nginx

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-APP_PORT=${PORT:-8000}
 cd /app/
+
+APP_PORT=${PORT:-8000}
+
+echo "Collecting static files..."
+/opt/virtualenv/bin/python manage.py collectstatic --noinput
+
+echo "Running migrations..."
 /opt/virtualenv/bin/python manage.py migrate
+
+echo "Starting the server..."
 /opt/virtualenv/bin/gunicorn --worker-tmp-dir /dev/shm mysite.wsgi:application --bind "0.0.0.0:${APP_PORT}"
--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DJANGO_DEBUG")) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
