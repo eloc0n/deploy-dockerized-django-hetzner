95a1615934ed86679112834b2297e36eafd42796
Giannis Bratias <giannis.bratias@grapevine.gr>
2023-07-29 22:20:03 +0300

    Remove unnecessary code

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -3,14 +3,3 @@
 echo "Acticating environment"
 source /opt/virtualenv/bin/activate
 exec "$@"
-
-KEYGEN=/usr/bin/ssh-keygen
-KEYFILE=/root/.ssh/id_rsa
-
-if [ ! -f $KEYFILE ]; then
-  $KEYGEN -q -t rsa -N "" -f $KEYFILE
-  cat $KEYFILE.pub >> /root/.ssh/authorized_keys
-fi
-
-echo "== Use this private key to log in =="
-cat $KEYFILE
