b875cc2baaad6d7183bfcf3fd04f87c3731d964d
Giannis Bratias <giannis.bratias@grapevine.gr>
2023-07-29 22:28:25 +0300

    Add collectstatic in scrip

--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -4,6 +4,9 @@ set -e
 echo "Dropping DB"
 ./manage.py reset_db --noinput
 
+echo "Collecting static files..."
+./manage.py collectstatic --noinput
+
 echo "Running migrations"
 ./manage.py migrate
 
